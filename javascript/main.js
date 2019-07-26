8239ab5a7cdd03538bdb057bb1751eb8aefadc46
csslick <digitone@naver.com>
2019-07-26 12:12:21 +0900

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,7 @@ $(function(){
   // 슬라이더
   var img_num = 0;
   var img_height = 100; // 높이
-  var duration = 1000;
+  var duration = 3000;
 
   setInterval(function(){
     if(img_num >= 3){ img_num = 0;}
