76fc5475266ef07f6a84ad202305d412b8737bb0
csslick <digitone@naver.com>
2019-07-26 13:03:15 +0900

    탭버튼 목록

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,31 @@
 // main.js
 $(function(){
+  // 탭 버튼
+  $('#btn-l').on('click', function(){
+    // 기존 목록들 초기화
+    $('#notice ul').removeClass('show');
+    // 현재 목록을 표시
+    $('#notice-list').addClass('show');
+
+    // 버튼 선택 스타일
+    $('.tab-group > a').removeClass('show');
+    $(this).addClass('show');
+    return false;
+  })
+  $('#btn-r').on('click', function(){
+    // 기존 목록들 초기화
+    $('#notice ul').removeClass('show');
+    // 현재 목록을 표시
+    $('#gallery-list').addClass('show');
+    
+    // 버튼 선택 스타일
+    $('.tab-group > a').removeClass('show');
+    $(this).addClass('show');    
+    return false;
+  })
+
+
+
   // 슬라이더
   var img_num = 0;
   var img_height = 100; // 높이
