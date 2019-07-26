ee7a66a10cc638e5bf3467c0dcc8ca3f70bac9ad
csslick <digitone@naver.com>
2019-07-26 12:07:02 +0900

    이미지 슬라이더 구현

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,19 @@
 // main.js
+$(function(){
+  // 슬라이더
+  var img_num = 0;
+  var img_height = 100; // 높이
+  var duration = 1000;
+
+  setInterval(function(){
+    if(img_num >= 3){ img_num = 0;}
+    $('.sliders').css({
+      top: -(img_height * img_num) + '%'
+    });
+    img_num++;
+  }, duration);
+
+
+
+
+}) // end $()
