3743e0a87a6d5d0f7309370542ab4b3ae145cd6c
csslick <digitone@naver.com>
2021-11-02 12:14:15 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,11 +1,12 @@
 // main.js
 $(function(){
+  // 2단계 메뉴 hover
   $('#gnb .depth1 > li').hover(
     function(){
-      $(this).find('.depth2').fadeIn();
+      $(this).find('.depth2').stop().fadeIn();
     },
     function(){
-
+      $(this).find('.depth2').stop().fadeOut();
     }
   )
 
